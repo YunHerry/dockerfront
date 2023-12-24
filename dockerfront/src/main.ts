6a612dcf08bcac6834faf90f7e78288ab2e24710
云荷YunHerry <54676890+YunHerry@users.noreply.github.com>
2023-12-24 23:27:13 +0800

    fix cannot start

--- dockerfront/src/main.ts
+++ dockerfront/src/main.ts
@@ -8,5 +8,6 @@ import 'vue-web-terminal/lib/theme/dark.css'
 import Terminal from "vue-web-terminal";
 import echarts from 'echarts';
 const app = createApp(App);
+//@ts-ignore
 app.use(router).use(Terminal).use(ElementPlus).mount("#app");
 app.config.globalProperties.$echarts = echarts;
