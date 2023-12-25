e593e87c42fdd3ec397610826041a61ec3e67a65
trusted <936796603@qq.com>
2023-12-25 23:01:04 +0800

    add image entity

---
+++ dockerfront/src/types/image.ts
@@ -0,0 +1,11 @@
+declare interface image {
+    createdAt: string;
+    updatedAt: string;
+    id: string;
+    tag:string;
+    repository:string;
+    imageId:string;
+    size:number;
+    unit:string;
+    author:number;
+}
